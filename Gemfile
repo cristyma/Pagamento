f5046e97a23699e3bc5da8639bc4ba0a20f9699a
cristiany maeda <cristyma@gmail.com>
2011-10-07 11:22:12 -0300

    primeiro commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+#gem 'sqlite3', group: :developmente
+#gem 'heroku', group: :developmente
+
+group :development	do
+	gem 'sqlite3'
+	gem 'heroku'
+end
+
+gem 'pg', group: :production
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+gem 'therubyracer' # prepara para uso do java coffe script
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pagamento::Application.config.secret_token = 'b158f3fc0d1251d27ffb3e409f1115cd2b17f49b07e51e0aa2e318169d1c40110ca20ef6a5f96f3dcc36aa0d1275802142e0863c7a0faca09e62531aea8e7f41'
