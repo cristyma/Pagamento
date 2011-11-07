e06468201a035abc16e04e5c5a8400393a960a10
cristiany maeda <cristyma@gmail.com>
2011-11-07 09:12:48 -0200

    tipo de usuarios

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Usuario < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
@@ -8,4 +10,16 @@ class Usuario < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :papel
 
   belongs_to :papel # é belongs_to pq tem a chave estrangeira na table ***_id
+
+  def admin?
+    papel.nome == 'Administrador'
+  end  
+
+  def usuario?
+    papel.nome == 'Usuário'
+  end  
+
+  def visitante?
+    papel.nome == 'Visistante'
+  end  
 end
